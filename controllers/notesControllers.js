cc2ed21e72ccc51cad81daabbed0746a36936b10
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-04-04 14:07:45 +0530

    Created addNote controller

--- controllers/notesControllers.js
+++ controllers/notesControllers.js
@@ -5,7 +5,15 @@ const notesControllers = {};
 
 notesControllers.addNote = async (req, res) => {
   const userId = req.user.id;
-  return res.json(userId);
+  const { title, content } = req.body;
+
+  try {
+    await Notes.create({ title, content, UserId: userId });
+    res.status(200).json({ message: "Note created successfully" });
+  } catch (errors) {
+    console.log("Error on addNotes", errors);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = notesControllers;
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -3,6 +3,6 @@ const authorize = require("../middleswares/authorize");
 const notesControllers = require("../controllers/notesControllers");
 const notesRoutes = express.Router();
 
-notesRoutes.get("/addNotes", authorize, notesControllers.addNote);
+notesRoutes.post("/addNotes", authorize, notesControllers.addNote);
 
 module.exports = notesRoutes;
